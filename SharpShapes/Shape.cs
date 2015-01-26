f78269adca29f2b107d9de281b1396cfed4c7bc9
CharisseL <charisse8@gmail.com>
2015-01-25 22:01:23 -0600

    thru 4 tests

--- SharpShapes/Shape.cs
+++ SharpShapes/Shape.cs
@@ -7,40 +7,45 @@ using System.Drawing;
 
 namespace SharpShapes
 {
-    public abstract class Shape
+    abstract public class Shape
     {
-        // Width, Height, Perimeter, Area, BorderColor, FillColor, NumberOfSides
         /// <summary>
-        /// Specifies the interior color of the shape.
+        /// The color of the interior of the shape, when drawn.
         /// </summary>
         public Color FillColor { get; set; }
 
         /// <summary>
-        /// Specifies the color of the border of the shape, when drawn.
+        /// The color of the border of the shape, when drawn.
         /// </summary>
         public Color BorderColor { get; set; }
-        
+
         /// <summary>
         /// The number of sides of this shape.
         /// </summary>
-        public virtual int SidesCount { get;  }
+        abstract public int SidesCount { get; }
+
+        public Shape()
+        {
+            BorderColor = Color.Tomato;
+            FillColor = Color.Bisque;
+        }
 
         /// <summary>
         /// Calculates the area of the shape.
         /// </summary>
         /// <returns>the area of the shape</returns>
-        public abstract decimal Area() {}
-        
-        /// <summary>
-        ///Calculates the perimetert of the shape 
-        /// </summary>
-        /// <returns>the perimeter of the shape</returns>
-        public abstract int Perimeter() {}
+        abstract public decimal Area();
 
         /// <summary>
-        /// Scales the shape in size
+        /// Calculates the perimeter of the shape.
+        /// </summary>
+        /// <returns>the perimeter of the shape</returns>
+        abstract public decimal Perimeter();
+
+        /// <summary>
+        /// Scales the shape in size.
         /// </summary>
         /// <param name="percent">the percentage by which to scale the shape</param>
-        public abstract void scale(int percent) { }
+        abstract public void Scale(int percent);
     }
 }
