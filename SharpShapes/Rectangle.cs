89145b502c9c8b2358fc74635c91dbcc7d0696f6
CharisseL <charisse8@gmail.com>
2015-01-26 01:14:04 -0600

    rectangle tests

---
+++ SharpShapes/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShapes
+{
+    public class Rectangle : Shape
+    {
+        private decimal width;
+        public decimal Width
+        {
+            get { return this.width; }
+        }
+
+        private decimal height;
+        public decimal Height
+        {
+            get { return this.height; }
+        }
+
+        public override int SidesCount
+        {
+            get { return 4; }
+        }
+
+        public Rectangle(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.width = width;
+            this.height = height;
+        }
+
+        public override decimal Area()
+        {
+            return Height * Width;
+        }
+
+        public override decimal Perimeter()
+        {
+            return 2 * Height + 2 * Width;
+        }
+
+        public override void Scale(int percent)
+        {
+            if (percent <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.width = width * percent / 100;
+            this.height = height * percent / 100;
+        }
+    }
+}
